21eef84a909703540acde2ab5cb58ecc2b9f695e
jimmy.fortin@gmail.com <jimmy.fortin@gmail.com>
2016-01-23 08:30:19 -0500

    :: is replaced by localhost when server is running

--- client-server-one-project/server/src/server.js
+++ client-server-one-project/server/src/server.js
@@ -11,5 +11,9 @@ var server = app.listen(9000, function() {
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log('Example app listening at http://%s:%s', host, port);
+    if (host === '::') {
+        host = 'localhost';
+    }
+
+    console.log('Server app listening at http://%s:%s', host, port);
 });
