9797d6dfd68da700ab757142615585e589829544
jimmy.fortin@gmail.com <jimmy.fortin@gmail.com>
2016-02-13 17:36:41 -0500

    New tasks manager used

--- client-server-one-project/gulpfile.js
+++ client-server-one-project/gulpfile.js
@@ -2,6 +2,4 @@ var gulp = require('gulp');
 var config = require('./config');
 var gulpAppBuildTasks = require('gulp-app-build-tasks');
 
-gulpAppBuildTasks.use(gulp);
-gulpAppBuildTasks.configure(config);
-gulpAppBuildTasks.registerTasks();
+gulpAppBuildTasks.apply(config, gulp);
